880e5a56598493021cd279f2efe8fa93bb974b66
kevin Agudelo Betancourt <kevinagudelobetancourt@macbook-pro-de-kevin.local>
2016-03-09 22:56:15 -0500

    Initial Commit

---
+++ iOSChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSChat
+//
+//  Created by kevin Agudelo Betancourt on 9/03/16.
+//  Copyright © 2016 kevin Agudelo Betancourt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
