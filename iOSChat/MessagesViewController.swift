98d4e73b5e8e7f39097df34b2cbbd6f9051ab211
kevin Agudelo Betancourt <kevinagudelobetancourt@MacBook-Pro-de-Kevin.local>
2016-03-15 12:50:37 -0500

    LaunchScreen and Chat Storyboards were creted

---
+++ iOSChat/MessagesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.swift
+//  iOSChat
+//
+//  Created by kevin Agudelo Betancourt on 9/03/16.
+//  Copyright © 2016 kevin Agudelo Betancourt. All rights reserved.
+//
+
+import Cocoa
+
+class MessagesViewController: UIViewController {
+
+}
